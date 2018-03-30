38e6bc7441f2d664c1ef62563d1440bf61e2fa49
Jo Engreitz <jo.engreitz@gmail.com>
2018-03-30 09:15:21 -0400

    Refactor: Manage app location with <Link> component

--- src/MyBooks.js
+++ src/MyBooks.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 class MyBooks extends Component {
   render() {
@@ -11,7 +12,7 @@ class MyBooks extends Component {
           <div className="list-books-content">
           </div>
           <div className="open-search">
-            <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+            <Link className="open-search" to='/search'>Add a book</Link>
           </div>
         </div>
         <div className="bookshelf">
