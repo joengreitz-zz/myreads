13096374e6cb173e28d942c360569b8b1c2a80a7
Jo Engreitz <jo.engreitz@gmail.com>
2018-04-02 12:49:40 -0400

    Chore: Added PropTypes to Book component

--- src/Book.js
+++ src/Book.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Book extends Component {
+  static propTypes = {
+    book: PropTypes.object.isRequired,
+  }
 
 /*TODO: Handle books with no author*/
 
