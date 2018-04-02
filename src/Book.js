3e5172d396506bdb4edea9c6042cb32c591d2276
Jo Engreitz <jo.engreitz@gmail.com>
2018-04-02 12:48:19 -0400

    Refactor: Add props as constant in render method

--- src/Book.js
+++ src/Book.js
@@ -8,12 +8,13 @@ class Book extends Component {
 /*TODO: Fix error when no image thumbnail*/
 
   render () {
+    const {book, shelfChange} = this.props
     return(
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book.imageLinks.thumbnail}")`}}></div>
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")`}}></div>
           <div className="book-shelf-changer">
-            <select defaultValue={this.props.book.shelf} onChange={(event) => this.props.shelfChange(this.props.book, event.target.value)}>
+            <select defaultValue={book.shelf} onChange={(event) => shelfChange(book, event.target.value)}>
               <option value="none" disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
@@ -22,8 +23,8 @@ class Book extends Component {
             </select>
           </div>
         </div>
-        <div className="book-title">{this.props.book.title}</div>
-        <div className="book-authors">{this.props.book.authors}</div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{book.authors}</div>
       </div>
   )
   }
