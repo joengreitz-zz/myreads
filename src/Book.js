d6d146c54e339ba281ac70c2b78d90a72392f9d1
Jo Engreitz <jo.engreitz@gmail.com>
2018-04-02 12:08:44 -0400

    Refactor: Pass book component book shelf and shelfChange as props

--- src/Book.js
+++ src/Book.js
@@ -1,30 +1,43 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+class Book extends Component {
+
+
 const Book = ({ book, onShelfChange, onBookClick }) => {
   Book.propTypes = {
     book: PropTypes.object.isRequired,
     onShelfChange: PropTypes.func.isRequired,
   }
 
-  return(
-    <div className="book">
-      <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
-        <div className="book-shelf-changer">
-          <select value={book.shelf} onChange={(e) => onShelfChange(book, e.target.value)}>
-            <option value="none" disabled>Move to...</option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
-            <option value="none">None</option>
-          </select>
+/*TODO: Handle books with no author*/
+
+
+/*TODO: Fix error when no image thumbnail*/
+
+  render () {
+    return(
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+          <div className="book-shelf-changer">
+            <select defaultValue={this.props.book.shelf} onChange={(event) => this.props.shelfChange(this.props.book, event.target.value)}>
+              <option value="none" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
         </div>
+        <div className="book-title">{book.title}</div>
+        <div className="book-authors">{book.authors}</div>
       </div>
-      <div className="book-title">{book.title}</div>
-      <div className="book-authors">{book.authors}</div>
-    </div>
-)
+  )
+  }
+
+
+  }
 }
 
 export default Book;
